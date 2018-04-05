64af00cc4cfc12c63c8ab866b2f2747af52613a4
Damnjot <236475@student.pwr.pl>
2018-04-05 16:29:42 +0200

    created get and post methods

--- src/main/java/com/testproject/controllers/ItemController.java
+++ src/main/java/com/testproject/controllers/ItemController.java
@@ -1,14 +1,25 @@
 package com.testproject.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/item")
+@RequestMapping("/api/items")
 public class ItemController {
 
+    @GetMapping
+    public String get(){
+        return "item";
+    }
+
+    @PostMapping
+    public String post(){
+        return "itempost";
+    }
     @PutMapping
     public String put(){
         return "put-method";
